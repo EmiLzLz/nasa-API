4d15fe3c2132984b1728f581c4cb7d011601a67a
emiliano <contact@emilianoll.com>
2023-01-20 19:16:34 -0600

    second commit

--- js/index.js
+++ js/index.js
@@ -9,13 +9,13 @@ const showData = (data) => {
   let imgInfo = document.getElementById("image-info");
   let imgTitle = document.getElementById("img-title");
   document.getElementById("img").src = data.hdurl;
+  document.getElementById("view").href = data.hdurl;
 
   imgTitle.innerHTML = `
     <h5 class="fs-1">${data.title}</h5>
     `;
 
   imgInfo.innerHTML = `
-    <p class="card-text">${data.explanation}</p>
-    <a href="${data.hdurl}" target="_blank" class="btn btn-primary fw-bold">VIEW FULL IMAGE</a> 
+    <p class="card-text">${data.explanation}</p> 
     `;
 };
